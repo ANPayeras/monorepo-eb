51d093368d46043cf5b83bf8b4756f6dbd2f0fd9
Angel Nicolas Payeras <anpayeras@teco.com.ar>
2025-06-09 17:40:23 -0300

    add rewrites

--- abm/next.config.mjs
+++ abm/next.config.mjs
@@ -22,25 +22,25 @@ const nextConfig = {
       {
         source:
           "/:user((?!build|templates|profile|layouts|sign-in|sign-up|api)[^/]+)",
-        destination: `http://localhost:3001/:user`,
+        destination: `${process.env.REWRITES_VIEW_URL}/:user`,
       },
       {
         source:
           "/:user((?!build)[^/]+)/:path(all|combo|confirmation)/:combo(1|2|3|4)?",
-        destination: `http://localhost:3001/:user/:path?combo=:combo`,
+        destination: `${process.env.REWRITES_VIEW_URL}/:user/:path?combo=:combo`,
       },
       {
         source: "/test/:user((?!build|templates|layout)[^/]+)",
-        destination: `http://localhost:3001/test/:user`,
+        destination: `${process.env.REWRITES_VIEW_URL}/test/:user`,
       },
       {
         source:
           "/test/:user((?!build|templates|layout)[^/]+)/:path(all|combo|confirmation)/:combo(1|2|3|4)?",
-        destination: `http://localhost:3001/test/:user/:path?combo=:combo`,
+        destination: `${process.env.REWRITES_VIEW_URL}/test/:user/:path?combo=:combo`,
       },
       {
         source: "/view/_next/:path+",
-        destination: `http://localhost:3001/view/_next/:path+`,
+        destination: `${process.env.REWRITES_VIEW_URL}/view/_next/:path+`,
       },
     ];
   },
