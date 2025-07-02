5f4481fc731f6da5d85931a51ba3bc2399a13725
Angel Nicolas Payeras <anpayeras@teco.com.ar>
2025-07-02 20:09:58 -0300

    test logs

--- view/src/components/combo.tsx
+++ view/src/components/combo.tsx
@@ -8,6 +8,8 @@ import useSentEvent from '@/hooks/use-sent-events'
 const Combo = ({ template, combo }: { template: Doc<"templates">, combo: string }) => {
     const { cart, handleOnChangeCart, handleOnChangeCartQuantity } = useDataStore(state => state)
     const { sentEvent } = useSentEvent()
+    console.log(template)
+    console.log(combo)
     const { description, price, title, imgUrl, id } = template.combos.find((c => c.id === combo))!
     const cartItem = cart.find(i => i.category === combo)
 
